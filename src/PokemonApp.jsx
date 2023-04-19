d33e5009b491d2c1db97b5d5402bf46debac6843
LucasNF22 <lucasnfiorentino@gmail.com>
2023-04-19 15:16:56 -0300

    Se implemento la visualizacion de los pokemons en forma paginada

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,27 +1,43 @@
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { getPokemons } from './store/slices/pokemon';
+import { useDispatch, useSelector } from 'react-redux'
+import { pokemonSlice, setPokemons } from './store/slices/pokemon/pokemonSlice';
+import { getPokemons } from './store/slices/pokemon/thunks';
+
 
 export const PokemonApp = () => {
 
     const dispatch = useDispatch();
+
+    const { isLoading, pokemons = [], page } = useSelector( state => state.pokemons )
 
     useEffect(() => {
 
         dispatch( getPokemons() )
     }, [])
 
+    console.log(pokemons);
 
   return (
     <>
         <h1>Pokemon App</h1>
+        Loading: { isLoading ? 'True' : 'False' }
         <hr />
+        
         <ul>
-            <li>Hola</li>
-            <li>Hola</li>
-            <li>Hola</li>
-            <li>Hola</li>
+          {
+            pokemons.map(pokemon =>(
+              <li key={pokemon.name} >{pokemon.name} </li>
+            ))
+          }
+            
         </ul>
+
+        <button
+          disabled= { isLoading }
+          onClick={ () => dispatch( getPokemons(page) ) }
+        >
+          Siguiente
+        </button>
     </>
   )
 }
--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -12,9 +12,9 @@ export const getPokemons = ( page = 0 ) => {
         // const data = await resp.json();
          
         // Usando axios
-        const { data } = await pokemonApi.get('/pokemon?limit=10&offset=0${ page * 10 }')
+        const { data } = await pokemonApi.get(`/pokemon?limit=10&offset=0${ page * 10 }`)
         
-        console.log( data );
+        // console.log( data );
 
         dispatch( setPokemons({ pokemons: data.results, page: page + 1 }) )
 

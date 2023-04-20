a1f9ed64cf2b8a818a87cb885a8ee654fbecb120
LucasNF22 <lucasnfiorentino@gmail.com>
2023-04-20 12:42:06 -0300

    Obtencion de todo por ID, utilizando custom hook creado por RTK

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
-import { useGetTodosQuery } from './store/api/todosApi'
+import React, { useState } from 'react'
+import { useGetTodoByIdQuery, useGetTodosQuery } from './store/api/todosApi'
 
 export const TodoApp = () => {
 
-    const { data: todos = [], isLoading } = useGetTodosQuery();
-    console.log(todos);
+    const [ todoId, setTodoId] = useState(1);
+
+    // const { data: todos = [], isLoading } = useGetTodosQuery();
+    const { data: todo, isLoading } = useGetTodoByIdQuery( todoId );
+
+    const nextTodo = () => {
+        setTodoId( todoId + 1 );
+    }
+    const prevTodo = () => {
+        if ( todoId === 1) return;
+        setTodoId( todoId - 1 );
+    }
 
     return (
         <>
             <h1>Todos - RTK Query</h1>
             <hr />
-            <h4>isLoading: { isLoading ? 'True' : 'Flase' } </h4>
+            <h4>isLoading: { isLoading ? 'True' : 'False' } </h4>
 
-            <pre>...</pre>
+            <pre>{ JSON.stringify( todo ) }</pre>
 
-            <ul>
+            {/* <ul>
                 {
                     todos.map( todo => (
                         <li key={todo.id} >
@@ -22,9 +32,12 @@ export const TodoApp = () => {
                         </li>
                     ) )
                 }
-            </ul>
+            </ul> */}
 
-            <button>
+            <button onClick={ prevTodo } >
+                Prev TODO
+            </button>
+            <button onClick={ nextTodo } >
                 Next TODO
             </button>
         </>
